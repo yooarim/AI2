e20cbdeacdda950f98c79b3be15f3692afe384a4
yooarim <20242317@sdhs.gwe.hs.kr>
2024-11-22 14:59:33 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,7 @@ def display_right_content(labels):
     # 3rd Row - Text based on labels
     for i, label in enumerate(labels[:3]):
         with cols[i]:
-            st.write(f"{label} 관련 텍스트 내용입니다.")
+            st.write(f"{label} 백승호는 잘생겼다")
 
 # 모델 로드
 st.write("모델을 로드 중입니다. 잠시만 기다려주세요...")
